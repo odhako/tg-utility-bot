e5e5e7ebf26f8c6b718ae02c7bb541a954af1973
odhako <odhako@gmail.com>
2025-10-02 23:05:54 +0300

    Change library to telethon

--- bot.py
+++ bot.py
@@ -1,36 +1,38 @@
-import asyncio
-import logging
 from dotenv import load_dotenv
 import os
+from telethon import TelegramClient, events
+import logging
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+
+# logging - set INFO of WARNING
+logging.basicConfig(format='[%(levelname)s %(asctime)s] %(name)s: %(message)s', level=logging.DEBUG)
 
 
-# loading from .env
+# log in
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-
-# logging
-logging.basicConfig(level=logging.INFO)
-
-# bot object
-TOKEN = os.environ.get('TOKEN')
-bot = Bot(TOKEN)
-
-# dispatcher
-dp = Dispatcher()
+api_id = int(os.environ.get('api_id'))
+api_hash = os.environ.get('api_hash')
+bot_token = os.environ.get('bot_token')
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 
-# handlers
-@dp.message(Command('start'))
-async def cmd_start(message: types.Message):
-    await message.answer('Hello, world!')
+# single message
+# with bot:
+#     bot.loop.run_until_complete(bot.send_message('@odhako', 'Hello, myself!'))
+#     bot.loop.run_until_complete(bot.send_file('@odhako', 'test_files/001.jpg'))
 
+# many messages
+# async def main():
+#     await bot.send_message('odhako', 'many test1')
+#     await bot.send_message('odhako', 'many test2')
+#     await bot.send_message('odhako', 'many test3')
+# with bot:
+#     bot.loop.run_until_complete(main())
 
-# polling
-async def main():
-    await dp.start_polling(bot)
+@bot.on(events.NewMessage)
+async def my_event_handler(event):
+    if 'hello' in event.raw_text:
+        await event.reply('hi!')
 
-
-if __name__ == '__main__':
-    asyncio.run(main())
+bot.start()
+bot.run_until_disconnected()
